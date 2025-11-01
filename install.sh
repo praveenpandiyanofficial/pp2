52cd63cbf0650aff541f701014b3c82ab40583e4
Praveenpandiyan <158056279+praveenpandiyanofficial@users.noreply.github.com>
2025-11-01 14:55:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
-
-# PP2 installer
-
-PP2_URL="https://raw.githubusercontent.com/praveenpandiyanofficial/pp2/main/pp2.sh"
-INSTALL_PATH="/usr/local/bin/pp2"
+# install.sh
 
 echo "Installing PP2..."
 
-# Download pp2.sh
-curl -fsSL $PP2_URL -o /tmp/pp2.sh
-if [ $? -ne 0 ]; then
-    echo "Failed to download pp2.sh from GitHub."
-    exit 1
-fi
+PP2_URL="https://raw.githubusercontent.com/praveenpandiyanofficial/pp2/main/pp2.sh"
+DEST="/usr/local/bin/pp2"
 
-# Move to /usr/local/bin and make executable
-sudo mv /tmp/pp2.sh $INSTALL_PATH
-sudo chmod +x $INSTALL_PATH
+curl -fsSL $PP2_URL -o $DEST
+chmod +x $DEST
 
-echo "PP2 installed successfully! You can now use it with the command: pp2"
+echo "PP2 installed! You can now run 'pp2' from anywhere."
